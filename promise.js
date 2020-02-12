608b59c6f06f2ecbbd4892e7b667cbd5c1c05a2a
ConnorKeddington <60441302+ConnorKeddington@users.noreply.github.com>
2020-02-12 13:54:32 -0700

    Update promise.js

--- promise.js
+++ promise.js
@@ -27,3 +27,7 @@ getBeers()
 
 
 main()
+
+//A promise is an object that puts out a value, but not immediately.
+//Promises are used when we have multiple operations going.
+//Promises help you handle multiple operations and identify errors easier
